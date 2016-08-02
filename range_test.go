96c370f6470adea3883b2dbecbe1a111ffe45aa9
Markus Sonderegger <marraison@gmail.com>
2016-08-02 16:59:16 +0200

    add missing copyright notice

--- range_test.go
+++ range_test.go
@@ -1,3 +1,8 @@
+// Copyright ©2012 The bíogo Authors. All rights reserved.
+// Copyright ©2016 Markus Sonderegger. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package llrb
 
 import (
